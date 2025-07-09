47a410eb78495cfc10610bfed9d690c4021623dc
Pronay Guha <57461706+pronayguha13@users.noreply.github.com>
2025-07-09 18:54:43 +0530

    implemented routes and app components with navigation (#4)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
-import LeagueTable from "@/features/League-Table";
+import { Link } from "react-router";
+
+import { Button } from "@/components/ui/button";
 
 const App = () => {
-  return <LeagueTable />;
+  return (
+    <div className="flex flex-col">
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+        La-Liga Analysis
+      </h1>
+      <div className="flex flex-row justify-center gap-4">
+        <Button variant="outline" size="lg" className="cursor-pointer" asChild>
+          <Link to="/league-table">Get league table</Link>
+        </Button>
+        <Button variant="outline" size="lg" className="cursor-pointer" asChild>
+          <Link to="/goal-stat">Get season wise goal stat</Link>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/features/Goal-Stats/index.tsx
+++ frontend/src/features/Goal-Stats/index.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useMemo, useState } from "react";
+
+import TableView from "@/components/TableView";
+import { getSeasonWiseGoalStat } from "@/features/Goal-Stats/service";
 const GoalStat = () => {
-  return <div>GoalStat</div>;
+  const [seasonWiseGoalData, setSeasonWiseGoalData] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    const fetchSeasonWiseGoalData = async () => {
+      const data = await getSeasonWiseGoalStat();
+
+      setSeasonWiseGoalData(data);
+    };
+
+    fetchSeasonWiseGoalData();
+  }, []);
+
+  const columns = useMemo(() => {
+    if (seasonWiseGoalData.length) {
+      const row = seasonWiseGoalData[0];
+
+      return Object.keys(row);
+    } else {
+      return [];
+    }
+  }, [seasonWiseGoalData]);
+
+  return (
+    <TableView
+      title="Season wise goal stat"
+      data={seasonWiseGoalData}
+      columns={columns}
+    />
+  );
 };
 
 export default GoalStat;
